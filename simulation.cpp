adc207c5d84326a6183e09502d288b6c61bd1d84
gabrielborgesc <jimmyrclgjf@gmail.com>
2020-04-18 13:00:32 -0300

    terminei, mas há erros de contagem de tempo> Falta testar e consertar bugs

--- simulation.cpp
+++ simulation.cpp
@@ -24,6 +24,7 @@ public:
     int tiq1 = 0; ///tempo em que o processo entrou na fila q1
     int qes; /// quantidade total de operações de E/S do processo
     int res; /// qauntidade restante de operações de E/S do processo
+    int term; ///instante em que processo terminou toda sua execução
     filas f; ///fila à qual pertence o processo
 
     void ex() { ///método que represnta execução de 1ms do processo
@@ -74,6 +75,8 @@ public:
 };
 
 class fila1 : public fila0 { ///classe para fila Q1
+private:
+    static const int tmaxq1 = 30;
 public:
     void update(int idex) { ///recebe id do processo em execução
         for(int i=0; i<p.size(); i++){
@@ -82,24 +85,52 @@ public:
             }
         }
     }
+
+    void elapsedTime(fila0 &q0){
+        vector <proc> paux;
+        int i, j;
+        for(i=1; i<p.size(); i++){ ///armazenando os processos de Q1 que chegaram a 30ms de espera
+            if(p[i].tq1 == tmaxq1){
+                paux.push_back(p[i]);
+            }
+        }
+
+        for(i=0; i<paux.size(); i++){ ///retirando esses processos da fila Q1
+            for(j=0; j<p.size(); j++){
+                if(paux[i].id == p[j].id){
+                    p.erase(p.begin()+j);
+                    break;
+                }
+            }
+        }
+
+        for(i=0; i<paux.size(); i++){ ///inserindo esses processos em Q0
+            q0.ins(paux[i]);
+        }
+
+    }
+
 };
 
 class filaes : public fila0 { ///classe para fila de E/S
 private:
-    static const int totales = 20; ///duração de todas as op de E/S
+    static const int totales = 2; ///duração de todas as op de E/S
 
 public:
      bool fex() { ///método que verifica fim de op de E/S
         return p[0].tes == totales;
     }
 
-    void exes() {
-        p[0].tes = t - p[0].ties;
-    }
-
     void update() {
         if(!p.empty())
         p[0].tes = t - p[0].ties;
+    }
+
+    void moveEStoQ0(fila0 &q0){
+        proc paux = p[0];
+        paux.res--;
+        p.erase(p.begin());
+        q0.ins(paux);
     }
 };
 
@@ -118,43 +149,48 @@ public:
         return p[0].f == Q1 && fex();
     }
 
-    bool intpriority(fila0 q0) { ///método que indica se processo em execução é de Q1 e foi interrompido porque chegou processo em Q0
+    bool stop(fila0 q0) { ///método que indica se processo em execução é de Q1 e foi interrompido porque chegou processo em Q0
         return !p.empty() && !q0.empty() && (p[0].id != q0.p[0].id) && (p[0].f = Q1);
     }
 
     void update() {
-        if(!p.empty())
+        if(!p.empty()){
         p[0].tex = t - p[0].tiex;
         p[0].rbcpu--;
+    }
     }
 
     void moveQ0toQ1(fila0 &q0, fila1 &q1){
         q0.pop();
         proc paux = p[0];
-        p.pop_back();
+        p.erase(p.begin());
+        paux.tiq1 = t;
         q1.ins(paux);
     }
 
-    void moveQ0toES(fila0 &q0, filaes &es){
+    void moveQtoES(fila0 &q0, filaes &es){
         q0.pop();
         proc paux = p[0];
-        p.pop_back();
+        p.erase(p.begin());
+        paux.ties = t;
         es.ins(paux);
     }
 
-    void moveQ1toES(fila1 &q1, filaes &es){
-        q1.pop();
+    void moveQtoTerm(fila0 &q0, fila0 &term){
+        q0.pop();
         proc paux = p[0];
-        p.pop_back();
-        es.ins(paux);
+        p.erase(p.begin());
+        paux.term = t;
+        term.ins(paux);
     }
 
     void replaceInQ1(fila1 &q1){ ///método para atualizar parâmetros de um processo que sai da fila de ex mas permanece em Q1
         proc paux = p[0];
-        p.pop_back();
+        p.erase(p.begin());
 
         for(int i=0; i<q1.size(); i++){
             if(q1.p[i].id == paux.id){
+                paux.tiq1 = t;
                 q1.p[i] = paux;
                 break;
             }
@@ -163,9 +199,41 @@ public:
 
 };
 
+
+void print(fila0 q0, fila1 q1, filaex ex, filaes es, fila0 term){
+int i;
+cout<<endl;
+cout<<"t = "<<t<<":"<<endl;
+cout<<"em execucao: ";
+for(i=0; i<ex.size(); i++){
+    cout<<"   P"<<ex.p[i].id;
+}
+
+cout<<endl<<"Fila Q0:";
+for(i=0; i<q0.size(); i++){
+    cout<<"   P"<<q0.p[i].id;
+}
+
+cout<<endl<<"Fila Q1:";
+for(i=0; i<q1.size(); i++){
+    cout<<"   P"<<q1.p[i].id;
+}
+
+cout<<endl<<"Fila E/S:";
+for(i=0; i<es.size(); i++){
+    cout<<"   P"<<es.p[i].id;
+}
+
+cout<<endl<<"Fila term:";
+for(i=0; i<term.size(); i++){
+    cout<<"   P"<<term.p[i].id;
+}
+cout<<endl;
+}
+
 int main () {
 
-int i, x, qtproc; ///quantidade total de processos
+int i, x, idex, qtproc; ///quantidade total de processos
 
 fila0 q0; ///fila Q0
 fila1 q1; ///fila Q1
@@ -177,15 +245,14 @@ vector <int> dscpu; ///vetor com a duração de brust cpu de cada processo
 vector <int> qesp; ///vetor com a quantidade de op de E/S de cada processo
 proc aux;
 
-
-while(1){
+/// pegando os dados iniciais
 cout<<"Quantidade de processos: ";
 cin>>qtproc;
 
 cout<<endl<<endl;
 
 for(i=0; i<qtproc; i++){ ///pegar duração de burst de cada processo
-    cout<<"Duracao do surtos de cpu do processo "<<i<<": ";
+    cout<<"Duracao do surtos de cpu do processo "<<i+1<<": ";
     cin>>x;
     dscpu.push_back(x);
     }
@@ -193,7 +260,7 @@ for(i=0; i<qtproc; i++){ ///pegar duração de burst de cada processo
 cout<<endl<<endl;
 
 for(i=0; i<qtproc; i++){ ///pegar qtd de E/S de cada processo
-    cout<<"Quantidade de operacoes de E/S do processo "<<i<<": ";
+    cout<<"Quantidade de operacoes de E/S do processo "<<i+1<<": ";
     cin>>x;
     qesp.push_back(x);
     }
@@ -207,48 +274,85 @@ for(i=0; i<qtproc; i++){ ///criando os processos e adicionando-os à fila Q0
 }
 
 cout<<endl<<endl;
+int cont = 0;
+while(1){
+cont++;
+if(cont==10) break;
+if(q0.empty() && q1.empty() && es.empty()) break;
 
 ///atualizando tempos de execução/espera
 ex.update();
-q1.update(ex.p[0].id);
+
+if(!ex.empty()){
+idex = ex.p[0].id;
+}
+else{
+idex = -1;
+}
+q1.update(idex);
+
 es.update();
 
+///tratando ex
 if(ex.empty()){ ///ninguém está em execução
     if(!q0.empty()){
         aux = q0.p[0];
-        aux.tiex = t;
-        ex.ins(aux);
     }
     else if(!q1.empty()){
         aux = q1.p[0];
+    }
+
+    if(!q0.empty() || !q1.empty()){
+        aux.rbcpu = aux.bcpu;
         aux.tiex = t;
         ex.ins(aux);
     }
+        print(q0, q1, ex, es, term);
 }
 
-else { ///existe processo em execução
 
+
+else { ///existe processo em execução
+print(q0, q1, ex, es, term);
 if(ex.fexrr()){ ///processo que está em execução é de Q0 e terminou por RR
 ex.moveQ0toQ1(q0, q1); ///move processo da fila Q0 para Q1
 }
 
 else if(ex.fexQ0()){ ///processo que está em execução é de Q0 e temrinou por bcpu
-ex.moveQ0toES(q0, es); ///move processo da fila Q0 para executar operação de E/S
-
-
-///aqui falta conferir se o processo ainda tem E/S pra executar. Se n, mover pra term
-
- }
+if(ex.p[0].res!=0){
+ex.moveQtoES(q0, es); ///move processo da fila Q0 para executar operação de E/S
+}
+else{
+ex.moveQtoTerm(q0, term); ///move processo da fila Q0 para fila de terminados
+}
+}
 
 else if(ex.fexQ1()){ ///processo que está em execução é de Q1 e temrinou por bcpu
-ex.moveQ1toES(q1, es); ///move processo da fila Q1 para executar operação de E/S
+if(ex.p[0].res!=0){
+ex.moveQtoES(q1, es); ///move processo da fila Q1 para executar operação de E/S
+}
+else{
+ex.moveQtoTerm(q1, term); ///move processo da fila Q1 para fila de terminados
+}
 }
 
-else if(ex.intpriority()){ ///processo que está em execução é de Q1 e deve ser interrompido porque chegou processo em Q0
-ex.replaceInQ1(q1); ///atualiza os parâmetros do processo em Q1
+else if(ex.stop(q0)){ ///processo que está em execução é de Q1 e deve ser interrompido porque chegou processo em Q0
+ex.replaceInQ1(q1); ///atualiza os parâmetros do processo em Q1 e retira processo de ex
 }
 
 }
+
+///tratando Q1
+q1.elapsedTime(q0); ///analisa se há processos em Q1 esperando há 30ms e os desloca de Q1 para Q0
+
+///tratando E/S
+if(!es.empty()){
+    if(es.fex()){ ///processo executando E/S terminou sua operação
+        es.moveEStoQ0(q0);
+    }
+}
+
+
 t++;
 }
 return 0;
--- teste.cpp
+++ teste.cpp
@@ -14,13 +14,17 @@ enum fila {
 int main () {
 
 vector <int> v;
-v.push_back(2);
+v.push_back(1);
 v.push_back(2);
 v.push_back(3);
-replace(v.begin(), v.end(),  2, 5);
+v.pop_back();
 
 for(int i=0; i<v.size(); i++){
     cout<<v[i]<<"  ";
+}
+
+for(i=0; i<5; i++){
+    cout<<i<<endl;
 }
 
 cout<<endl<<endl;
